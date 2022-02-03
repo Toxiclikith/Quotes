e0edc109d60131922840cbd426a09ededd357aff
Joy~Toxic <75122082+Toxiclikith@users.noreply.github.com>
2022-02-03 19:42:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Quotes
+ Random Quotes Genetrator
---
+++ script.js
@@ -0,0 +1,28 @@
+const quoteText = document.querySelector(".quote"),
+authorName = document.querySelector(".author .name"),
+quoteBtn = document.querySelector("button"),
+soundBtn = document.querySelector(".sound"),
+copyBtn = document.querySelector(".copy"),
+twitterBtn = document.querySelector(".twitter");
+function randomQuote(){
+    quoteBtn.classList.add("loading");
+    quoteBtn.innerText = "Loading Quote...";
+    fetch("https://api.quotable.io/random").then(res => res.json()).then(result =>{
+        quoteText.innerText = result.content;
+        authorName.innerText = result.author;
+        quoteBtn.innerText = "New Quote";
+        quoteBtn.classList.remove("loading");
+    });
+}
+soundBtn.addEventListener("click", ()=>{
+    let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+    speechSynthesis.speak(utterance);
+});
+copyBtn.addEventListener("click", ()=>{
+    navigator.clipboard.writeText(quoteText.innerText);
+});
+twitterBtn.addEventListener("click", ()=>{
+    let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+    window.open(tweetUrl, "_blank");
+});
+quoteBtn.addEventListener("click", randomQuote);
